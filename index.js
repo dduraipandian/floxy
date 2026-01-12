e8ba0f553b9d26a5c70fec0b014764484538ee2a
Durai Pandian <dduraipandian@gmail.com>
2026-01-12 15:04:26 +0530

    fixing lint config

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ export { Flow } from "./src/flow.js";
 export { FlowCanvas } from "./src/components/canvas.js";
 export { DagValidator } from "./src/components/plugins/dag-validator.js";
 
+// eslint-disable-next-line import/extensions
 import "@uiframe/core/style";
+
+// eslint-disable-next-line import/extensions
 import "@uiframe/notification/style";
+
 import "./src/css/base.css";
