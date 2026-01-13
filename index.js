43f4b50434ca7760ee29a512a23d20c8dffdfc83
Durai Pandian <dduraipandian@gmail.com>
2026-01-13 12:42:04 +0530

    add css from cdn

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-export { notification } from "@uiframe/notification";
+import "./src/css/base.css";
 
 export { Flow } from "./src/flow.js";
-export { FlowCanvas } from "./src/components/canvas.js";
 export { DagValidator } from "./src/components/plugins/dag-validator.js";
-
-// eslint-disable-next-line import/extensions
-import "@uiframe/core/style";
-
-// eslint-disable-next-line import/extensions
-import "@uiframe/notification/style";
-
-import "./src/css/base.css";
